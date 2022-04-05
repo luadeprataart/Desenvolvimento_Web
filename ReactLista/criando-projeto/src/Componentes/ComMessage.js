21112a1ae31804f9db4dd63d423cdb3218515e49
Ana Julia Moraes <79658493+luadeprataart@users.noreply.github.com>
2022-04-05 20:42:18 -0300

    Create ComMessage.js

---
+++ ReactLista/criando-projeto/src/Componentes/ComMessage.js
@@ -0,0 +1,10 @@
+import Fila from './fila.js'
+
+export default function ComMessage (){
+    return ( 
+    <div> 
+        <h2> Cliente na Fila: </h2> 
+        <Fila></Fila>
+    </div> 
+    );
+}
