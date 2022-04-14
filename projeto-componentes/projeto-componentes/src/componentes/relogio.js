014a036c109ad76a22127afc6887321eb8877192
Ana Julia Moraes <79658493+luadeprataart@users.noreply.github.com>
2022-04-14 16:17:29 -0300

    Create relogio.js

---
+++ projeto-componentes/projeto-componentes/src/componentes/relogio.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function Relogio() {
+  const [date, setDate] = useState(new Date());
+
+  function refreshClock() {
+    setDate(new Date());
+  }
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
+  return <span>{date.toLocaleTimeString()}</span>;
+}
