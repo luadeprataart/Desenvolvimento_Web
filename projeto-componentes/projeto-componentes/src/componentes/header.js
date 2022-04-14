f7240d60a18f8f55f13abf49cf2f122e68157212
Ana Julia Moraes <79658493+luadeprataart@users.noreply.github.com>
2022-04-14 16:17:27 -0300

    Create header.js

---
+++ projeto-componentes/projeto-componentes/src/componentes/header.js
@@ -0,0 +1,12 @@
+import { AiFillAlert } from "react-icons/ai";
+
+export default function Header() {
+  return (
+    <>
+      <h1>
+        {" "}
+        Cadastro de Clientes: <AiFillAlert color="white" />
+      </h1>
+    </>
+  );
+}
