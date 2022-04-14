d97b3ed9531bb15673d933f82b398aeee2fba634
Ana Julia Moraes <79658493+luadeprataart@users.noreply.github.com>
2022-04-14 16:17:36 -0300

    Create index.js

---
+++ projeto-componentes/projeto-componentes/src/index.js
@@ -0,0 +1,17 @@
+
+import './index.css';
+import App from './App';
+import {createRoot} from "react-dom/client";
+import 'react-toastify/dist/ReactToastify.css';
+import{ToastContainer} from "react-toastify";
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <ToastContainer autoClose={false} />
+    <App tab="home"></App>
+   
+  </>
+);
+
+
